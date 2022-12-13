2e13dcb1d812e336d029419fff55455cf2c8e1f0
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-12-13 05:59:13 -0800

    implement stack and node

---
+++ monty.h
@@ -0,0 +1,40 @@
+#ifndef MONTY_H
+#define MONTY_H
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+/** the utlity function for monty, stack and queue*/
+stack_t *add_node(stack_t **stack, const int n);
+stack_t *queue_node(stack_t **stack, const int n);
+void free_stack(stack_t *stack);
+size_t print_stack(const stack_t *stack);
+#endif /**MONTY_H*/
